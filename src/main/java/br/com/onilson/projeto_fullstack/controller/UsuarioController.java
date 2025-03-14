9bc640e70979ea93881379dea8d04c2644adbb85
onilson-csf <onilson.cardoso@gmail.com>
2025-03-14 16:50:22 -0300

    refatoração na anotação do CrossOrigin

--- src/main/java/br/com/onilson/projeto_fullstack/controller/UsuarioController.java
+++ src/main/java/br/com/onilson/projeto_fullstack/controller/UsuarioController.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value = "/usuario")
-@CrossOrigin
+@CrossOrigin(origins = "*")
 public class UsuarioController {
 
     private final UsuarioService usuarioService;
